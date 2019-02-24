e76360af7b0d7ca775908cd3f8a97e59d2a90d4b
Katrin Shibakova <katrina.gubanova@gmail.com>
2019-02-24 21:05:20 +0300

    added card drawing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
 import {
-  renderFilters
-} from './filters/renderFilters.js';
+  createFiltersElements
+} from './filters/createFiltersElements.js';
 import {
   NamesFilterDict
 } from './filters/namesFilterDict.js';
+import {
+  createCardElement
+} from './card/createCardElement.js';
+import {
+  preparedData
+} from './data.js';
+
+
+const containerElementFilter = document.querySelector(`.trip-filter`);
+const containerCards = document.querySelector(`.trip-day__items`);
+
+
+const renderFilters = (filterElements) => {
+  containerElementFilter.innerHTML = ``;
+  return containerElementFilter.appendChild(createFiltersElements(filterElements));
+};
 
 renderFilters(NamesFilterDict);
+
+
+const createCardsFragment = (arrayCards) => {
+  let cardsFragment = document.createDocumentFragment();
+  for (let elementCards of arrayCards) {
+    cardsFragment.appendChild(createCardElement(elementCards));
+  }
+  return cardsFragment;
+};
+
+const renderBoardCards = (cardsFragment) => {
+  containerCards.innerHTML = ``;
+  return containerCards.appendChild(cardsFragment);
+};
+
+renderBoardCards(createCardsFragment(preparedData));
