e590797d50a17d52704709ddc0399011aa5387b6
Katrin Shibakova <katrina.gubanova@gmail.com>
2019-03-01 19:10:58 +0300

    event time is generated randomly, duration is calculated depending on time

--- src/data.js
+++ src/data.js
@@ -23,36 +23,35 @@ const destinations = [`Airport`,
 
 const prices = [10, 20, 40, 80];
 
-const times = [
-  {
-    time: {
-      start: `10.00`,
-      end: `11.00`
-    },
-    duration: `1h 30m`
-  },
-  {
-    time: {
-      start: `12.00`,
-      end: `15.30`
-    },
-    duration: `3h 30m`
-  },
-  {
-    time: {
-      start: `16.00`,
-      end: `17.00`
-    },
-    duration: `1h`
-  },
-  {
-    time: {
-      start: `18.30`,
-      end: `20.00`
-    },
-    duration: `1h 30m`,
+const getMinutes = () =>
+  getRandomInteger(0, 59) > 30 ? 30 : 0;
+
+const getTime = () => {
+  const startHour = getRandomInteger(0, 23);
+  const endHour = getRandomInteger(startHour + 1, 24);
+  return {
+    startHour,
+    startMinutes: getMinutes(),
+    endHour,
+    endMinutes: getMinutes()
+  };
+};
+
+const addZero = (number) =>
+  String(number).padStart(2, `0`);
+
+const parseTimeToString = ({startHour, startMinutes, endHour, endMinutes}) =>
+  `${addZero(startHour)}:${addZero(startMinutes)}&nbsp;&mdash; ${addZero(endHour)}:${addZero(endMinutes)}`;
+
+const getDuration = ({startHour, startMinutes, endHour, endMinutes}) => {
+  let hourDuration = endHour - startHour;
+  let minutesDuration = endMinutes - startMinutes;
+  if (minutesDuration < 0) {
+    hourDuration = hourDuration - 1;
+    minutesDuration = 60 + minutesDuration;
   }
-];
+  return `${hourDuration}h ${minutesDuration}m`;
+};
 
 const Specials = [`Order UBER`,
   `Switch to comfort class`,
@@ -64,11 +63,13 @@ const Specials = [`Order UBER`,
 let getDataCards = () => {
   let dataCards = [];
   for (let i = 1; i < 8; i++) {
+    const time = getTime();
     const dataCard = {
       id: i,
       type: types[getRandomInteger(0, types.length - 1)],
       destination: destinations[getRandomInteger(0, destinations.length - 1)],
-      time: times[getRandomInteger(0, times.length - 1)],
+      time: parseTimeToString(time),
+      duration: getDuration(time),
       price: prices[getRandomInteger(0, prices.length - 1)],
       specials: getMixedArray(Specials).slice(0, getRandomInteger(0, 2))
         .map((name) => ({name, price: getRandomInteger(10, 100)}))
